b906ceed51f63c193ae58f89ea0f1719c3e126ba
Piotr Michałowski <pmichalowski1@gmail.com>
2021-02-20 22:25:27 +0100

    Example 2

--- src/app/example2/components/location-form/location-form.component.spec.ts
+++ src/app/example2/components/location-form/location-form.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationFormComponent } from './location-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EPISODE_LOCATION_STUB } from '../../services/locations.service.stub';
+import { By } from '@angular/platform-browser';
 
 describe('NameFormComponent', () => {
   let component: LocationFormComponent;
@@ -10,7 +12,7 @@ describe('NameFormComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LocationFormComponent],
-      imports: [ReactiveFormsModule],
+      imports: [ReactiveFormsModule, ReactiveFormsModule],
     }).compileComponents();
   }));
 
@@ -22,5 +24,50 @@ describe('NameFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.form).toBeDefined();
+  });
+
+  it('should create the form based on input data', () => {
+    component.location = EPISODE_LOCATION_STUB;
+    component.ngOnInit();
+    expect(component.form.get('name').value).toEqual(EPISODE_LOCATION_STUB.name);
+    expect(component.form.get('id').value).toEqual(EPISODE_LOCATION_STUB.id);
+  });
+
+  describe('#locationChanged', () => {
+    beforeEach(() => {
+      spyOn(component.locationChanged, 'emit');
+      component.location = EPISODE_LOCATION_STUB;
+      component.ngOnInit();
+    });
+
+    // 1 way - updating the form through the view
+    it('should output the data on input value changes', () => {
+      const nameInput = fixture.debugElement.query(By.css('[data-selector=location-name-input]'));
+      nameInput.nativeElement.value = 'Lorem';
+      nameInput.nativeElement.dispatchEvent(new Event('input'));
+      expect(component.locationChanged.emit).toHaveBeenCalledWith(
+        jasmine.objectContaining({ name: 'Lorem' }),
+      );
+    });
+
+    // 2nd way - updating the form through the patchValue/setValue method
+    it('should output the data on form value changes', () => {
+      component.form.patchValue({ name: 'Ipsum' });
+      expect(component.locationChanged.emit).toHaveBeenCalledWith(
+        jasmine.objectContaining({ name: 'Ipsum' }),
+      );
+    });
+
+    it('should not output the data when the form value changed to invalid data', () => {
+      component.form.patchValue({ name: '' });
+      expect(component.locationChanged.emit).not.toHaveBeenCalled();
+    });
+
+    it('should not output the form data after the component was destroyed', () => {
+      component.ngOnDestroy();
+      component.form.patchValue({ name: 'FooBar' });
+      expect(component.locationChanged.emit).not.toHaveBeenCalled();
+    });
   });
 });
--- src/app/example2/components/location-form/location-form.component.ts
+++ src/app/example2/components/location-form/location-form.component.ts
@@ -1,17 +1,36 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EpisodeLocation } from '../../episode-location';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-location-form',
   templateUrl: './location-form.component.html',
   styleUrls: ['./location-form.component.scss'],
 })
-export class LocationFormComponent implements OnInit {
+export class LocationFormComponent implements OnInit, OnDestroy {
   @Input() location: EpisodeLocation;
   @Output() locationChanged = new EventEmitter<EpisodeLocation>();
+  form: FormGroup;
+  private formSubscription: Subscription;
 
   constructor(private builder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.builder.group({
+      id: this.builder.control(this.location?.id, Validators.required),
+      name: this.builder.control(this.location?.name, Validators.required),
+    });
+    this.formSubscription = this.form.valueChanges.subscribe(formData => {
+      if (this.form.valid) {
+        this.locationChanged.emit(formData);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.formSubscription) {
+      this.formSubscription.unsubscribe();
+    }
+  }
 }
